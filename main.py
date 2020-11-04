759c2f6f306da10832d306257494bbd4d6d8e342
kuriakosenamboothiri <73921422+kuriakosenamboothiri@users.noreply.github.com>
2020-11-04 08:30:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import youtube_dl
 import os
 location=input("Enter full download location")
 if location == "":
-  location = "/content/drive/"
+  location = "/content/"
 os.chdir(location)
 link = input("Enter Link : ")
 ydl_opts = {
